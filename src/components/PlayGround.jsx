c3791c4b529746f3fd42258ce72f188ea82eb581
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2023-12-14 21:29:38 +0530

    updated

--- src/components/PlayGround.jsx
+++ src/components/PlayGround.jsx
@@ -14,6 +14,8 @@ export const PlayGround = () => {
   const [randomNumber, setRandomNumber] = useState();
   const [userNumber, setUserNumber] = useState("");
   const [showRules, setShowRules] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+
 
   const rollDiceHandler = () => {
     if (userNumber === "") {
@@ -67,23 +69,80 @@ export const PlayGround = () => {
           </div>
           <div>
             <div>
-              <input
+              {/* <input
                 className='text-center md:text-2xl text-xl mt-2 md:mt-0 border-b-2 w-[280px] md:w-[350px] border-black outline-none'
                 type="text"
                 placeholder='Enter number Between 1 to 6'
                 onChange={(val) => { setUserNumber(val.target.value) }}
-              />
+              /> */}
+              <div className='flex justify-evenly text-xl'>
+                <div
+                  onClick={() => {
+                    setUserNumber(1);
+                    setSelectedNumber(1);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 hover:bg-[#565656] py-1 ${selectedNumber === 1 ? 'selected' : ''}`}
+                >
+                  1
+                </div>
+                <div
+                  onClick={() => {
+                    setUserNumber(2);
+                    setSelectedNumber(2);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 py-1 hover:bg-[#565656] ${selectedNumber === 2 ? 'selected' : ''}`}
+                >
+                  2
+                </div>
+                <div
+                  onClick={() => {
+                    setUserNumber(3);
+                    setSelectedNumber(3);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 py-1 hover:bg-[#565656] ${selectedNumber === 3 ? 'selected' : ''}`}
+                >
+                  3
+                </div>
+                <div
+                  onClick={() => {
+                    setUserNumber(4);
+                    setSelectedNumber(4);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 py-1 hover:bg-[#565656] ${selectedNumber === 4 ? 'selected' : ''}`}
+                >
+                  4
+                </div>
+                <div
+                  onClick={() => {
+                    setUserNumber(5);
+                    setSelectedNumber(5);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 py-1 hover:bg-[#565656] ${selectedNumber === 5 ? 'selected' : ''}`}
+                >
+                  5
+                </div>
+                <div
+                  onClick={() => {
+                    setUserNumber(6);
+                    setSelectedNumber(6);
+                  }}
+                  className={`cursor-pointer border-2 border-black px-2 py-1 hover:bg-[#565656] ${selectedNumber === 6 ? 'selected' : ''}`}
+                >
+                  6
+                </div>
+              </div>
             </div>
             <div className='text-center text-2xl'>Choose a number </div>
           </div>
+          
         </div>
         <div className=''>
-          <div className='text-xl mt-4 flex items-center justify-center'>
-            {<img   src={diceImages[randomNumber - 1]} alt="" width={100} />}
+          <div className='text-xl mt-4 flex items-center justify-center '>
+            {<img  src={diceImages[randomNumber - 1]} alt="" width={100} />}
           </div>
           <div className='flex items-center justify-center cursor-pointer ' onClick={rollDiceHandler} >
             <div className='border-3 border-sky-500 rounded-lg'>
-              <button className='bg-sky-400 px-4 py-2 rounded-lg'>Click here to roll dice</button>
+              <button className='bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce mt-2'>Click here to roll dice</button>
             </div>
           </div>
         </div>
